1df77ea0ccf7ef191d963712e8189c7baf78fb76
Valentin Zwerschke <vallezw@gmail.com>
2023-01-15 21:26:10 +0100

    Add outline

--- demo/main.c
+++ demo/main.c
@@ -119,7 +119,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        //mat4x4_rotate_Z(m, m, (float) glfwGetTime());
         mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         mat4x4_mul(mvp, p, m);
 
